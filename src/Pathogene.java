e991326c2bc3181e3244cbdaa8eb318f6dd300e3
mariemaytraud <maytraudmarie@gmail.com>
2025-12-05 15:46:09 +0100

    correction de Pathogene + ajout du constructeur et de la méthode calculerCharge

--- src/Pathogene.java
+++ src/Pathogene.java
@@ -1,13 +1,28 @@
 import java.util.Map;
 
-public class Pathogene {
-    private double L; 
-    private double taux; 
-    private double alpha; 
-    private Map<String,double> medicamentResistance ;
-    private Map<String,double> medicamentSensibilite; 
-    private double reactiviteB;
-    private int idPathogene ; 
-    //testttttt
+public abstract class Pathogene { //car on va implementer 
+    protected  double L; // pour l'héritage 
+    protected double taux; 
+    protected  double alpha; 
+    protected Map<String, Double> medicamentResistance ;
+    protected  Map<String, Double> medicamentSensibilite; 
+    protected  double reactiviteB;
+    protected  int idPathogene; 
+
+// Constructeur 
+public Pathogene(double L, double taux, double alpha, Map<String, Double> medicamentResistance, Map<String, Double> medicamentSensibilite, double reactiviteB, int idPathogene) {
+    this.L = L;
+    this.taux = taux;
+    this.alpha = alpha;
+    this.medicamentResistance = medicamentResistance;
+    this.medicamentSensibilite = medicamentSensibilite;
+    this.reactiviteB = reactiviteB;
+    this.idPathogene = idPathogene;
+
+}
+
+//methode calculerCharge 
+
+public abstract double calculerCharge(); // methode abstraite sans parametre car impl
 
 }
